989e25def5805f29a9592234e16c6702ce51b07a
unknown <cmsong>
2021-12-22 10:06:28 +0900

    refactor: moveHead/Tail시 나머지 연산으로 인덱스 구하도록 변경

--- circular_queue.go
+++ circular_queue.go
@@ -41,11 +41,7 @@ func (q CircularQueue) IsEmpty() bool {
 // 만약 head가 size보다 크면 head를 처음 위치로 이동한다.
 // 만약 head가 tail과 같으면 tail을 다음 위치로 이동한다.
 func (q *CircularQueue) moveHead() {
-	if q.head == q.maxIndex() {
-		q.head = 0
-	} else {
-		q.head++
-	}
+	q.head = (q.head + 1) % q.size
 
 	if q.head == q.tail {
 		q.moveTail()
@@ -55,13 +51,5 @@ func (q *CircularQueue) moveHead() {
 // moveTail는 tail을 다음 위치로 이동하고
 // 만약 tail이 size보다 크면 tail을 처음 위치로 이동한다.
 func (q *CircularQueue) moveTail() {
-	if q.tail == q.maxIndex() {
-		q.tail = 0
-	} else {
-		q.tail++
-	}
-}
-
-func (q CircularQueue) maxIndex() int {
-	return q.size - 1
+	q.tail = (q.tail + 1) % q.size
 }
